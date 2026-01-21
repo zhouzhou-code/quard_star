7fae264cdfdef587d770e6a65c776ec453df2b5b
zhouzhou_code <2031145985@qq.com>
2026-01-22 02:56:05 +0800

    仿照riscv-qemu设备移植了Kconfig,defconfig和boaed文件；只差设备树没编译通过，但是还是没把uboot跑起来。

--- board/emulation/qemu-quard-star/qemu-quard-star.c
+++ board/emulation/qemu-quard-star/qemu-quard-star.c
@@ -1,19 +1,23 @@
-// // SPDX-License-Identifier: GPL-2.0+
+// SPDX-License-Identifier: GPL-2.0+
 
-// #include <common.h>
-// #include <dm.h>
-// #include <env.h>
-// #include <fdtdec.h>
-// #include <image.h>
-// #include <log.h>
-// #include <init.h>
+#include <dm.h>
+#include <dm/ofnode.h>
+#include <env.h>
+#include <fdtdec.h>
+#include <image.h>
+#include <log.h>
+#include <spl.h>
+#include <init.h>
+#include <usb.h>
+#include <virtio_types.h>
+#include <virtio.h>
 
-// int board_init(void)
-// {
-// 	return 0;
-// }
+int board_init(void)
+{
+	return 0;
+}
 
-// int board_late_init(void)
-// {
-// 	return 0;
-// }
+int board_late_init(void)
+{
+	return 0;
+}
